659ac946f3b39f7a52657b115d12de22caefa7e0
Kent Salcedo <kentsalcedo@Kents-MacBook-Pro.local>
2016-01-06 14:21:18 -1000

    write fully formed header in response to GET request. will work on accessing file system and returning actual page

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ function serverConnection(socketReq){
 
   socketReq.on('data', function(buffer){
     console.log('connection made');
-    socketReq.write('HTTP/1.1 200 OK\n\n');
+    socketReq.write('HTTP/1.1 200 OK\n');
+    socketReq.write('Server: localhost/1.4.6 (Ubuntu)\n');
+    socketReq.write('Date: ' + date + '\n');
+    socketReq.write('Content-Type: text/html; charset=utf-8\n');
+    socketReq.write('Connection: keep-alive\n\n');
+
     // console.log(buffer.toString());
     socketReq.write('this should be in the body');
     return socketReq.end();
