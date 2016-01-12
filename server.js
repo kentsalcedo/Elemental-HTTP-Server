d117f5d8cd2d665b9c56556d3223f08c741ad955
Kent Salcedo <kentsalcedo@Kents-MacBook-Pro.local>
2016-01-11 14:58:50 -1000

    refactor server to move request handlers outside of main block of code

--- server.js
+++ server.js
@@ -5,57 +5,75 @@ var path = require('path');
 var querystring = require('querystring');
 var url = require('url');
 
+// set port and host
 var PORT = 8080;
 var HOST = 'localhost';
 
 // create instance of server
 var server = http.createServer(connection);
 
-// helper function to return error 404 page
-
-
 // handles server requests
 function connection(request, response) {
   var method = request.method;
   var pathName = request.url;
   var fileType = path.extname(pathName);
+    if ( pathName === '/') {
+      pathName = '/index.html';
+      fileType = 'html';
+    }
   var postObject;
   var newPost;
 
-  if ( method === 'PUT'){
-    console.log('PUT request');
+  console.log(method, ' method');
 
-    response.writeHead(200, {
-      'Content-Type' : "text/" + fileType.replace('.',''),
-    });
+  ///////// handles the GET request
+  if (method === 'GET'){
+    handleGetRequest(request,response,method,pathName,fileType);
+  }
 
-    response.end('{ success: true }');
-  } // end of response if PUT request
+  ///////// handles the POST request
+  if (method === 'POST'){
+    handlePostRequest(request,response,method,pathName,fileType);
+  }
 
-  if (method === "GET"){
-    response.writeHead(200, {
-      'Content-Type' : "text/" + fileType.replace('.','')
-    });
+  ///////// handles the PUT request
+  if (method === 'PUT'){
+    handlePutRequest(request,response,method,pathName,fileType);
+  }
 
-    if (pathName === '/'){
-      pathName = '/index.html';
-      response.writeHead(200, {
-        'Content-Type' : 'text/html'
-      });
-    }
+} // end of connection function
+
+// server listening
+server.listen({
+  host : HOST,
+  port : PORT
+  }, function(){
+  console.log('server listening');
+});
+
+////////////////////////////////////////////////////////////
+/////////////// handles GET requests ///////////////////////
+////////////////////////////////////////////////////////////
+function handleGetRequest(request,response,method,pathName,fileType){
 
     fs.readFile("./public" + pathName, function(err,data){
-      if (err) response.writeHead (404, 'File Not Found', {
-          'test-header' : 'error out'
-      });
+      if (err) {
+        return fs.readFile('./public/404.html', function(err, data){
+          if (err) console.log(err);
+          response.end(data);
+        });
+      }
       response.end(data);
-      });
-  } // end of response if GET request
+    });
 
-  if (method === "POST"){
+}
 
-    request.on('data', function(chunk){
-      var newElementForm = querystring.parse(chunk.toString());
+////////////////////////////////////////////////////////////
+///////////////// handles POST requests/////////////////////
+////////////////////////////////////////////////////////////
+function handlePostRequest(request,response,method,pathName,fileType){
+  request.on('data', function(data){
+      var newElementForm = querystring.parse(data.toString());
 
       // console.log('public/' + newElementForm.elementName +'.html');
 
@@ -95,8 +113,10 @@ function connection(request, response) {
               var currentIndexFile = data.toString().split('<');
               var newElementListItem = 'a href="./' + elementName + '.html">' + '<li>' + elementName.toUpperCase() + '</li></a>';
               currentIndexFile.splice(currentIndexFile.lastIndexOf('!--end of list -->')-2,0,newElementListItem);
+
+              // console.log(elementCount);
               newIndexFile = currentIndexFile.join('<');
-              // console.log(newIndexFile);
+
 
                 fs.writeFile('./public/index.html', newIndexFile, function(err,data){
                 if (err) console.log(err);
@@ -116,15 +136,54 @@ function connection(request, response) {
     }); // end of on data requests
 
   response.end();
-  } // end of POST
+} // end of POST
 
-} // end of connection function
+////////////////////////////////////////////////////////////
+///////////////// handles PUT requests//////////////////////
+////////////////////////////////////////////////////////////
 
+function handlePutRequest(request,response,method,pathName,fileType){
+  request.on('data', function(data){
+      var newElementForm = querystring.parse(data.toString());
 
-// server listening
-server.listen({
-  host : HOST,
-  port : PORT
-  }, function(){
-  console.log('server listening');
-});
+      // console.log('public/' + newElementForm.elementName +'.html');
+
+      response.writeHead(201, 'File Made', {
+        'Content-Type' : "text/" + fileType.replace('.','')
+      });
+
+      fs.access('public/' + newElementForm.elementName + '.html', fs.F_OK, function(err){
+      // fs.access('public/index.htm', fs.F_OK, function(err){
+
+        if (err) console.log('Cannot do PUT request, file does not exist') ;
+        // end of else, else is the situation that creates file on if doesn't exist yet
+
+        else {
+
+            var elementName = newElementForm.elementName;
+
+            fs.readFile('./public/template.html', function(err,data){
+              if(err) console.log(err);
+
+              newPost = data.toString();
+              newPost = newPost.replace( /template/gi,elementName);
+              newPost = newPost.replace(/ATOMICNUMBER/gi,newElementForm.elementAtomicNumber);
+              newPost = newPost.replace(/ELEMENTSYMBOL/gi,newElementForm.elementSymbol);
+              newPost = newPost.replace(/DESCRIPTION/gi,newElementForm.elementDescription);
+
+              fs.writeFile('public/' + elementName + '.html', newPost, function(err){
+                if(err) console.log('error');
+                console.log('file updated!');
+
+              }); // end of fs.write
+
+            }); // end of fs.readfile
+
+          }
+
+      });
+
+    }); // end of on data requests
+
+  response.end();
+} // end of POST
